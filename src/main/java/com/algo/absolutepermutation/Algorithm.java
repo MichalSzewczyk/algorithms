a30cb95af22d7091bb7efe34ff3c8c62f6e9f8d0
szewczykm <szewczykm94@gmail.com>
2020-02-09 17:22:32 +0100

    Added description for absolute permutation algorithm

--- src/main/java/com/algo/absolutepermutation/Algorithm.java
+++ src/main/java/com/algo/absolutepermutation/Algorithm.java
@@ -1,6 +1,13 @@
 package com.algo.absolutepermutation;
 
 public class Algorithm {
+    /**
+     * We define P to be a permutation of the first n natural numbers in the range [1, n].
+     * Let pos[i] denote the value at position i in permutation P using 1-based indexing.
+     * P is considered to be an absolute permutation if |pos[i] - i| = k holds true for every i in [1, n].
+     * Given n and k, print the lexicographically smallest absolute permutation P.
+     * If no absolute permutation exists, print -1.
+     */
     static int[] absolutePermutation(int n, int k) {
         int[] result = new int[n];
         boolean[] isUsed = new boolean[n + 1];
