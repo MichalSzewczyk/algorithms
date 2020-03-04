ce5494afb9453fc5108757688f14ed042a061429
szewczykm <szewczykm94@gmail.com>
2020-03-04 21:53:35 +0100

    Added description to algorithm

--- src/main/java/com/algo/bomberman/Main.java
+++ src/main/java/com/algo/bomberman/Main.java
@@ -3,6 +3,20 @@ package com.algo.bomberman;
 import java.util.Arrays;
 
 public class Main {
+    /**
+     * Each bomb can be planted in any cell of the grid but once planted,
+     * it will detonate after exactly 3 seconds. Once a bomb detonates,
+     * it's destroyed — along with anything in its four neighboring cells.
+     * This means that if a bomb detonates in cell (i, j), any valid cells (i +/- 1, j) and (i, j +/- 1) are cleared. If there is a bomb in a neighboring cell, the neighboring bomb is destroyed without detonating, so there's no chain reaction.
+     *
+     * Bomberman is immune to bombs, so he can move freely throughout the grid. Here's what he does:
+     *
+     * Initially, Bomberman arbitrarily plants bombs in some of the cells, the initial state.
+     * 1. After one second, Bomberman does nothing.
+     * 2. After one more second, Bomberman plants bombs in all cells without bombs, thus filling the whole grid with bombs. No bombs detonate at this point.
+     * 3. After one more second, any bombs planted exactly three seconds ago will detonate. Here, Bomberman stands back and observes.
+     * 4. Bomberman then repeats steps 3 and 4 indefinitely.
+     */
     static char[][] bomberMan(int n, char[][] grid) {
         if(n % 2 == 0) {
             return fillWithBombs(grid);
