815e200ae2180b107499480627e71faa43e364e1
szewczykm <szewczykm94@gmail.com>
2020-04-07 23:10:39 +0200

    Added description for the algorithm

--- src/main/java/com/algo/larrysarray/Algorithm.java
+++ src/main/java/com/algo/larrysarray/Algorithm.java
@@ -5,6 +5,11 @@ import java.util.SortedSet;
 import java.util.TreeSet;
 
 public class Algorithm {
+    /**
+     * Larry has been given a permutation of a sequence of natural numbers incrementing from 1 as an array.
+     * He must determine whether the array can be sorted using the following operation any number of times:
+     * Choose any 3 consecutive indices and rotate their elements in such a way that ABC -> BCA -> CAB -> ABC.
+     */
     static String larrysArray(int[] array) {
         NavigableSet<Integer> numbersTree = new TreeSet<>();
         int swaps = 0;
