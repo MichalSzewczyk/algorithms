ca055eb9484ae87ad4dd003e6d262481dda2b622
szewczykm <szewczykm94@gmail.com>
2020-09-17 23:26:39 +0200

    Added information about greedy algorithm

--- src/main/java/com/algo/maxguestlist/greedy/Main.java
+++ src/main/java/com/algo/maxguestlist/greedy/Main.java
@@ -1,10 +1,13 @@
-package com.algo.maxguestlist;
+package com.algo.maxguestlist.greedy;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
     /**
+     * Greedy implementation taking guest with smallest amount of excluded guests at first place.
+     * This implementation doesn't give correct results.
+     *
      * Problem
      * Given list of mutually exclusive guests.
      * Find the maximum guest list with respect to aforementioned exclusions.
@@ -20,10 +23,30 @@ public class Main {
      */
     public static void main(String[] args) {
         int[][] excludingPairs = new int[][]{
+//                {0, 4},
+//                {0, 5},
+//                {0, 6},
+//                {4, 1},
+//                {4, 2},
+//                {4, 3},
+//                {5, 1},
+//                {5, 2},
+//                {5, 3},
+//                {6, 1},
+//                {6, 2},
+//                {6, 3},
+//                {1, 2},
+//                {1, 3},
+//                {2, 3},
+
                 {0, 1},
                 {0, 2},
-                {2, 3},
+                {0, 3},
                 {1, 4},
+                {2, 5},
+                {3, 6},
+                {1, 2},
+                {2, 3},
         };
         Collection<Integer> guestList = computeGuestsCollection(excludingPairs);
         System.out.println(guestList);
