0dfbc6d44400a12082671eed0abe5f194654b818
szewczykm <szewczykm94@gmail.com>
2020-10-05 21:56:27 +0200

    Added description to ranges algorithm

--- src/main/java/com/algo/ranges/Algorithm.java
+++ src/main/java/com/algo/ranges/Algorithm.java
@@ -5,6 +5,36 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Algorithm {
+    /**
+     * Link: https://www.hackerrank.com/challenges/connected-cell-in-a-grid/problem
+     * Consider a matrix where each cell contains either a 0 or a 1. Any cell containing a 1 is called a filled cell.
+     * Two cells are said to be connected if they are adjacent to each other horizontally, vertically, or diagonally.
+     * In the following grid, all cells marked X are connected to the cell marked Y.
+     * XXX
+     * XYX
+     * XXX
+     * If one or more filled cells are also connected, they form a region.
+     * Note that each cell in a region is connected to zero or more cells in the region but is not necessarily
+     * directly connected to all the other cells in the region.
+     * Given an n x m matrix, find and print the number of cells in the largest region in the matrix.
+     * Note that there may be more than one region in the matrix.
+     * For example, there are two regions in the following 3 x 3 matrix. The larger region at the top left contains 3 cells.
+     * The smaller one at the bottom right contains .
+     * 110
+     * 100
+     * 001
+     *
+     * Sample Input:
+     * 4
+     * 4
+     * 1 1 0 0
+     * 0 1 1 0
+     * 0 0 1 0
+     * 1 0 0 0
+     *
+     * Sample Output:
+     * 5
+     */
     static int connectedCell(int[][] matrix) {
         List<int[]> nodesToVisit = new LinkedList<>();
         int maxDistanceWalked = 0;
