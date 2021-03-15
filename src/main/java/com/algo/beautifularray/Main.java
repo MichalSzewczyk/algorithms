a98b5432680d04f5a8da12d7eaf613571af01ab0
szewczykm <szewczykm94@gmail.com>
2021-03-15 23:34:37 +0100

    Added algorithm description

--- src/main/java/com/algo/beautifularray/Main.java
+++ src/main/java/com/algo/beautifularray/Main.java
@@ -6,6 +6,22 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
+    /**
+     * Link: https://www.hackerrank.com/challenges/lilys-homework/problem
+     * Whenever George asks Lily to hang out, she's busy doing homework. George wants to help her finish it faster,
+     * but he's in over his head! Can you help George understand Lily's homework so she can hang out with him?
+     *
+     * Consider an array of n distinct integers, arr = [a[0], a[1], ..., a[n - 1]].
+     * George can swap any two elements of the array any number of times. An array is beautiful if the sum of |arr[i] - arr[i - 1]| among 0 < i < n is minimal.
+     *
+     * Given the array arr, determine and return the minimum number of swaps that should be performed in order to make the array beautiful.
+     *
+     * Sample input:
+     * [2, 5, 3, 1]
+     *
+     * Expected output:
+     * 2
+     */
     private static final Scanner scanner = new Scanner(System.in);
 
     static int lilysHomework(int[] arr) {
