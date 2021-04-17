0d1041d2713c827db7545a82d8e11e75e480ed73
szewczykm <szewczykm94@gmail.com>
2021-04-17 17:03:48 +0200

    Added description to BestSum algorithm

--- src/main/java/com/algo/dp/BestSum.java
+++ src/main/java/com/algo/dp/BestSum.java
@@ -3,15 +3,18 @@ package com.algo.dp;
 import java.util.*;
 
 /**
- *
+ * Implement bestSum function, so that for given:
+ * int sum       - integer representing target sum
+ * int[] values  - array of integers to form target sum
+ * bestSum returns the smallest permutation with repetitions of integers from values array which sums to target sum.
  */
-public class HowSumDp {
+public class BestSum {
     public static void main(String[] args) {
-        int[] result = howSum(100, new int[]{1, 2, 5, 25});
+        int[] result = bestSum(100, new int[]{1, 2, 5, 25});
         System.out.println(Arrays.toString(result));
     }
 
-    private static int[] howSum(int sum, int[] values) {
+    private static int[] bestSum(int sum, int[] values) {
         Deque<Integer> sums = new LinkedList<>();
         Map<Integer, Integer> visited = new HashMap<>();
         sums.add(sum);
@@ -31,14 +34,14 @@ public class HowSumDp {
         return new int[]{};
     }
 
-    private static int[] getPath(Map<Integer, Integer> walkedSums, int sum) {
+    private static int[] getPath(Map<Integer, Integer> walked, int sum) {
         int currentSum = 0;
         Collection<Integer> result = new LinkedList<>();
         while (currentSum != sum) {
-            int nextSum = walkedSums.get(currentSum);
+            int nextSum = walked.get(currentSum);
             result.add(nextSum - currentSum);
             currentSum = nextSum;
         }
-        return result.stream().mapToInt(Integer::valueOf).toArray();
+        return result.stream().mapToInt(value -> value).toArray();
     }
 }
