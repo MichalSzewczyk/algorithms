cfa7a447b9736f88d4dd553ca03d4ca95bf5b2b3
szewczykm <szewczykm94@gmail.com>
2021-06-27 16:27:05 +0200

    Implemented BST

---
+++ src/main/java/com/algo/bst/Algorithm.java
@@ -0,0 +1,93 @@
+package com.algo.bst;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Algorithm {
+    public static void main(String[] args) {
+        BSTTree<String> tree = new BSTTree<>();
+
+        tree.addElement("a");
+        tree.addElement("c");
+        tree.addElement("d");
+        tree.addElement("b");
+
+        System.out.println(tree.getValues());
+    }
+
+    private static class BSTTree<T extends Comparable<T>> {
+        private Node root;
+
+        void addElement(T value) {
+            if (root == null) {
+                root = new Node(value);
+            } else {
+                Node currentNode = root;
+                Node previousNode = root;
+                while (currentNode != null) {
+                    previousNode = currentNode;
+                    if (currentNode.getValue().compareTo(value) < 0) {
+                        currentNode = currentNode.getRightChild();
+                    } else {
+                        currentNode = currentNode.getLeftChild();
+                    }
+                }
+                if (previousNode.getValue().compareTo(value) < 0) {
+                    previousNode.setRightChild(new Node(value));
+                } else {
+                    previousNode.setLeftChild(new Node(value));
+                }
+            }
+        }
+
+        List<T> getValues() {
+            Node currentNode = root;
+            List<T> values = new LinkedList<>();
+            getValuesRecursive(currentNode, values);
+            return values;
+        }
+
+        private void getValuesRecursive(Node currentNode, List<T> values) {
+            if (currentNode != null) {
+                getValuesRecursive(currentNode.getLeftChild(), values);
+                values.add(currentNode.getValue());
+                getValuesRecursive(currentNode.getRightChild(), values);
+            }
+        }
+
+        private class Node implements Comparable<Node> {
+            private final T value;
+            private Node leftChild;
+            private Node rightChild;
+
+            public Node getLeftChild() {
+                return leftChild;
+            }
+
+            public Node getRightChild() {
+                return rightChild;
+            }
+
+            public void setLeftChild(Node leftChild) {
+                this.leftChild = leftChild;
+            }
+
+            public void setRightChild(Node rightChild) {
+                this.rightChild = rightChild;
+            }
+
+            public Node(T value) {
+                this.value = value;
+            }
+
+            public T getValue() {
+                return value;
+            }
+
+            @Override
+            public int compareTo(Node node) {
+                return value.compareTo(node.getValue());
+            }
+        }
+    }
+}
