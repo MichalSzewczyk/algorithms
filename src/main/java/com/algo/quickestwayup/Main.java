3d7d882d57ba981f55a25b3089122f1d488f2ebf
szewczykm <szewczykm94@gmail.com>
2021-11-01 19:35:22 +0100

    Added sample input and output for quickest way up algorithm

--- src/main/java/com/algo/quickestwayup/Main.java
+++ src/main/java/com/algo/quickestwayup/Main.java
@@ -22,6 +22,40 @@ public class Main {
      * - If a player lands at the base of a ladder, the player must climb the ladder. Ladders go up only.
      * - If a player lands at the mouth of a snake,
      * the player must go down the snake and come out through the tail. Snakes go down only.
+     * <p>
+     * Sample Input:
+     * 2
+     * 3
+     * 32 62
+     * 42 68
+     * 12 98
+     * 7
+     * 95 13
+     * 97 25
+     * 93 37
+     * 79 27
+     * 75 19
+     * 49 47
+     * 67 17
+     * 4
+     * 8 52
+     * 6 80
+     * 26 42
+     * 2 72
+     * 9
+     * 51 19
+     * 39 11
+     * 37 29
+     * 81 3
+     * 59 5
+     * 79 23
+     * 53 7
+     * 43 33
+     * 77 21
+     * <p>
+     * Sample Output:
+     * 3
+     * 5
      */
     public static final int BOARD_SIZE = 100;
 
