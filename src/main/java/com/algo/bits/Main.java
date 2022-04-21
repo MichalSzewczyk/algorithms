7b36388e3ff0934bf854e3f2fdc624c5814e8bfd
szewczykm <szewczykm94@gmail.com>
2022-04-21 23:41:14 +0200

    Implemented algorithm to count number of flips required for the sum of two binary numbers to be equal to target sum

--- src/main/java/com/algo/bits/Main.java
+++ src/main/java/com/algo/bits/Main.java
@@ -12,11 +12,14 @@ public class Main {
         System.out.println("Bits counted: " + setBitsCount);
         setBitsCount = countSetBitsOptimized(7);
         System.out.println("Bits counted optimized: " + setBitsCount);
+        int flipsCount = countFlipsToGetSum(4, 4, 12);
+        System.out.println("Flips required: " + flipsCount);
     }
 
     private static boolean isEven(int number) {
         return ((number & 1) == 0);
     }
+
 
     private static char upperCase(char character) {
         return (char) (character & ~32);
@@ -44,5 +47,42 @@ public class Main {
             counter++;
         }
         return counter;
+    }
+
+    private static int countFlipsToGetSum(int firstNumber, int secondNumber, int targetSum) {
+        int flips = 0;
+        boolean isOverflow = false;
+        for (int bit = 0; bit < 32; bit++) {
+            int firstNumberBit = firstNumber >> bit & 1;
+            int secondNumberBit = secondNumber >> bit & 1;
+            int targetSumBit = targetSum >> bit & 1;
+            int currentBit;
+            if ((firstNumberBit & secondNumberBit) == 1) {
+                if (isOverflow) {
+                    currentBit = 1;
+                } else {
+                    currentBit = 0;
+                }
+                isOverflow = true;
+            } else if ((firstNumberBit | secondNumberBit) == 1) {
+                if (isOverflow) {
+                    currentBit = 0;
+                    isOverflow = true;
+                } else {
+                    currentBit = 1;
+                }
+            } else {
+                if (isOverflow) {
+                    currentBit = 1;
+                    isOverflow = false;
+                } else {
+                    currentBit = 0;
+                }
+            }
+            if (currentBit != targetSumBit) {
+                flips++;
+            }
+        }
+        return flips;
     }
 }
